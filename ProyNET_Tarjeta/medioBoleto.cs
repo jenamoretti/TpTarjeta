5225f333e5ca5798f84267887a3e01d08c2e26a5
galoberlini <galoberlini@gmail.com>
2024-11-01 17:50:42 -0300

    Ref 13# : Implementacion franquicias

--- ProyNET_Tarjeta/medioBoleto.cs
+++ ProyNET_Tarjeta/medioBoleto.cs
@@ -4,42 +4,48 @@ using System.Linq;
 
 namespace Iteraciones
 {
-class MedioBoleto : Tarjeta
+    public class MedioBoleto : Tarjeta
     {
-        //Atributos
+        // Atributos
         private float desc;
-        
-        public override float Descuento_franquicia
-    {
-        get { return desc; }
-        set { desc = value; }
-    }
 
-        //Constructor 
-        public MedioBoleto(int saldo) : base(saldo){
+        public override float Descuento_franquicia
+        {
+            get { return desc; }
+            set { desc = value; }
+        }
+
+        // Constructor
+        public MedioBoleto(int saldo) : base(saldo)
+        {
             desc = 0.5f;
         }
-        //Métodos
-        public override bool EsPosibleViajar(int tarifa){
-            if(saldo + 480 < tarifa)
-                {return false;}
-            Boleto ult_bol = boletos_pagos.LastOrDefault();
-            if(ult_bol != null){
-                TimeSpan dif = DateTime.Now - ult_bol.GetFecha();
-                if(dif.TotalMinutes < 5)
-                    {return false;}
-                else
-                    {return true;}
-                }
-            else 
-                return true;
-            }  
-        public override void CalcularFranquicia(){
-            if (cant_usos_hoy < 4)
-                Descuento_franquicia = 0.5f;
-            else 
-                Descuento_franquicia = 1.0f;  
 
+        // Métodos
+        public override bool EsPosibleViajar(int tarifa)
+        {
+            if (Saldo + 480 < tarifa || !EsHorarioValido())
+                return false;
+
+            Boleto ult_bol = boletos_pagos.LastOrDefault();
+            if (ult_bol != null)
+            {
+                TimeSpan dif = Tarjeta.tiempo.Now() - ult_bol.GetFecha();
+                if (dif.TotalMinutes < 5)
+                    return false;
+                else
+                    return true;
             }
+            else
+                return true;
+        }
+
+        public override void CalcularFranquicia()
+        {
+            if (CantUsosHoy < 4)
+                Descuento_franquicia = 0.5f;
+            else
+                Descuento_franquicia = 1.0f;
+        }
     }
 }
